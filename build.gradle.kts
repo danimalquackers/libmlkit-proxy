b42c23f8306a8d3d81fc4dcf6e3ce32aa5ee73f1
Daniel Hellstern <git@daniel.hellstern.org>
2026-06-25 22:20:03 -0700

    fix: updated deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.library") version "8.2.2"
-    id("org.jetbrains.kotlin.android") version "1.9.22"
+    id("org.jetbrains.kotlin.android") version "2.1.0"
     
     // Keep the versions plugin for future checks
     id("com.github.ben-manes.versions") version "0.51.0"
@@ -33,10 +33,11 @@ dependencies {
     implementation("org.json:json:20260522")
 
     // ML Kit GenAI APIs
+    implementation("com.google.mlkit:common:18.11.0")
     implementation("com.google.mlkit:genai-prompt:1.0.0-beta2")
     implementation("com.google.mlkit:genai-speech-recognition:1.0.0-alpha1")
 
     // Coroutines for blocking NanoHTTPD threads safely
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
